76217e9e6424fb466dffcfd54f17c5add9ee1728
Johnson <jzwang52249320@gmail.com>
2024-12-18 13:42:32 +0800

    gcc -o main main.c -I/opt/homebrew/include

--- leetcode_1/main.c
+++ leetcode_1/main.c
@@ -1,9 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include "uthash.h"
 
 //Using UT_hash libery.
 
 #define TABLE_SIZE 10000
+
+struct my_struct{
+    int id;
+    char name[10];
+    UT_hash_handle hh;
+};  
 
 typedef struct element{
     struct bucket *head; 
@@ -15,14 +22,14 @@ typedef struct bucket{
     struct bucket *next;   
 } bucket;
 
-int *twoSum(int *nums, int numSize, int target, int *returnSize){
+void  twoSum(int *nums, int numSize, int target, int *returnSize){
     element *hash_Table = (element *)malloc(sizeof(element)*TABLE_SIZE);
     for(int i=0; i<TABLE_SIZE; ++i){
         hash_Table[i].head = NULL;
         hash_Table[i].tail = NULL;
     }
     
-     
+    return;     
 }
 
 int main(){
